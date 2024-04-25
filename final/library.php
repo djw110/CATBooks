518bffa43ba373f39d071905791d03c0215b2ac5
Daniel Whooley <danieljwhooley@gmail.com>
2024-04-25 12:15:14 -0400

    Final Project: worked on the detail page.

---
+++ final/library.php
@@ -0,0 +1,44 @@
+<?php
+    include 'top.php';
+?>
+
+        <main>
+            <h2 class="centered">
+            Library
+            </h2>
+            <div class="mainsection">
+                    <section class="libsection">
+                        <?php
+                                $sql = 'SELECT pmkBookID, fldTitle, fldLastName, fldYear, varRating FROM tblBooks';
+                                $statement = $pdo->prepare($sql);
+                                $statement->execute();
+
+                                $books = $statement->fetchAll();
+
+                                foreach($books as $book){
+                                    print '<a href="detail.php?id=' . $book['pmkBookID'] .  '" class="booklink">';
+                                    print '<article class = "book">';
+                                    print '<h3 class="title">' . $book['fldTitle'] . '</h3>';
+                                    print '<p class="author">' . $book['fldLastName'] . '</p>';
+                                    print '<p class="date"><em>' . $book['fldYear'] . '</em></p>';
+                                    if($book['varRating'] > 0){
+                                        print '<p class="rating">' . $book['varRating'] . '</p>';
+                                    }
+                                    else {
+                                        print '<p class="rating">Unrated</p>';
+                                    }
+                                    print'</article>';
+                                    print '</a>' . PHP_EOL;
+                                }
+                                ?>
+                        
+                        
+                    </section>
+            </div>
+        </main>
+
+        <?php
+            include 'footer.php';
+        ?>
+    </body>
+</html>
